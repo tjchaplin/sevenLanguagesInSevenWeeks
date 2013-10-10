25d13fc53a907496b0e35fd429143ce211cb31fc
tjchaplin <tjchaplin@hotmail.com>
2013-10-10 22:23:20 +0100

    chapter1

---
+++ printNumber.rb
@@ -0,0 +1,7 @@
+def printTo(maxNumber)
+	for i in 1..maxNumber
+		puts "This is sentence number #{i}"
+	end
+end
+
+printTo(10)
---
+++ times.rb
@@ -0,0 +1,9 @@
+class String
+	def times(multiplier)
+		puts "lasdjf"
+		result = (self + " ") * multiplier
+		puts "#{result}"
+	end
+end
+
+"Tim Chaplin".times(10)
